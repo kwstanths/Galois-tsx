b6f4b7595fb43d4c152f9960b9d6d01dbe028289
Konstantinos Kazatzis <kon.kazatzis@gmail.com>
2017-07-30 19:51:40 +0300

    final commit

---
+++ results/code_folder/bigtransaction_singlelock/delaunayrefinement/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/users/kkazatzis/Galois-2.2.1/apps/delaunayrefinement
+# Build directory: /home/users/kkazatzis/Galois-2.2.1/build/release/apps/delaunayrefinement
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
